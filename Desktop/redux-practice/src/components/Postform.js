23e0e71a71313d38a193fe08b3caa3cc9fc24a56
Casey <caseyclinga@gmail.com>
2020-10-29 15:19:16 -0700

    Added post form

---
+++ Desktop/redux-practice/src/components/Postform.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Postform extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form>
+          <div>
+            <label>Title:</label>
+            <br />
+            <input type='text' />
+          </div>
+          <div>
+            <label>Body:</label>
+            <br />
+            <textarea name='body' />
+          </div>
+          <br />
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Postform;
--- Desktop/redux-practice/src/components/Posts.js
+++ Desktop/redux-practice/src/components/Posts.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((data) => this.setState({ posts: data }));
+  }
+
   render() {
-    return <div></div>;
+    const postItems = this.state.posts.map((post) => (
+      <div key={post.id}>
+        <h1>{post.title}</h1>
+        <p>{post.body}</p>
+      </div>
+    ));
+    return (
+      <div>
+        <h1>{postItems}</h1>
+      </div>
+    );
   }
 }
 
